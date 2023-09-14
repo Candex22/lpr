e718f3532051277872cc46bb1fb86e670b997c30
mayorgamaximo <mayorgamaximo5@gmail.com>
2023-09-14 17:10:18 -0300

    sag

--- ae.py
+++ ae.py
@@ -2,8 +2,6 @@ from tkinter import *
 from datetime import *
 from tkinter import messagebox
 import mysql.connector
-from tkcalendar import Calendar
-
 
 
 ventana = Tk()
@@ -43,7 +41,6 @@ def actualizar_estado_tarea(tarea, checkbox):
 
 # Funcion para mostrar los campos de entrada (botón 1)
 def mostrar_campos_de_entrada():
-    global cal
     # Borrar cualquier widget
     for widget in texto.winfo_children():
         widget.destroy()
@@ -55,39 +52,55 @@ def mostrar_campos_de_entrada():
     descripcion_label.pack()
     descripcion_entry.pack()
     descripcion_entry.pack(pady=10)
-    today = datetime.today()
+
     fecha_vencimiento_label.pack()
-    cal = Calendar(ventana, selectmode='day', year=today.year, month=today.month,day=today.day)
-    cal.pack()
-    fecha_vencimiento_label.pack(pady=10)
+    fecha_vencimiento_entry.pack()
+    fecha_vencimiento_entry.pack(pady=10)
 
     submit.pack()
 
-
 # Funcion para agregar tareas a la lista
 def agregar_tarea_a_lista():
-    titulo = str(titulo_entry.get()).strip()
-    descripcion = str(descripcion_entry.get()).strip()
+    titulo = str(titulo_entry.get()).strip()  # Eliminar espacios en blanco
+    descripcion = str(descripcion_entry.get()).strip()  # Eliminar espacios en blanco
+    fecha_vencimiento_str = str(fecha_vencimiento_entry.get())
 
     if not titulo or not descripcion:
         messagebox.showerror("Error", "Tanto el título como la descripción deben contener al menos un caracter.")
         return
+    try:
+        # Analizar la fecha ingresada
+        fecha_vencimiento = datetime.strptime(fecha_vencimiento_str, "%Y-%m-%d")
 
-    selected_date = cal.get_date()  # Get the selected date from the calendar
-    print(selected_date)
-    hoy = str(datetime.now().date())
-    if selected_date >= hoy:
-        tarea = Tarea(titulo, descripcion, selected_date)
-        lista_tareas.append(tarea)
-        titulo_entry.delete(0, END)
-        descripcion_entry.delete(0, END)
-    else:
-        messagebox.showerror("Error", "La fecha debe ser mayor o igual a la fecha actual.")
+        # Obtener la fecha actual
+        hoy = datetime.now().date()
 
+        # Verificar si la fecha ingresada es mayor o igual a la fecha actual
+        if fecha_vencimiento.date() >= hoy:
+            tarea = Tarea(titulo, descripcion, fecha_vencimiento)
+            lista_tareas.append(tarea)
+            # Limpiar los campos de entrada
+            titulo_entry.delete(0, END)
+            descripcion_entry.delete(0, END)
+            fecha_vencimiento_entry.delete(0, END)
+        else:
+            messagebox.showerror("Error", "La fecha debe ser mayor o igual a la fecha actual.")
+    except ValueError:
+        messagebox.showerror("Error", "Fecha no válida. Use el formato AAAA-MM-DD.")
+    
+    #cargo los datos en la base de datos
+    iniciobd()
+    
+    sql="insert into tareas(titulo, descripcion,fecha_venc,user) values (%s,%s,%s,%s)"
+    datos=(titulo,descripcion,fecha_vencimiento_str,usuario_login.get())
+    cursor1.execute(sql, datos)
+    conexion1.commit()
+
+    cierrebd()
+    
 
 # Funcion para mostrar las tareas (boton 2)
 def mostrar_tareas():
-    cal.pack_forget()
     # Borrar cualquier widget previo
     for widget in texto.winfo_children():
         widget.destroy()
@@ -98,13 +111,14 @@ def mostrar_tareas():
             tarea_texto += f"Título: {tarea.titulo}\n"
             tarea_texto += f"Descripción: {tarea.descripcion}\n"
             tarea_texto += "Estado: Incompleto\n"
-            tarea_texto += f"Fecha de Vencimiento: {tarea.fecha_vencimiento}\n\n"
+            tarea_texto += f"Fecha de Vencimiento: {tarea.fecha_vencimiento.strftime('%Y-%m-%d')}\n\n"
         else:
             tarea_texto += f"Tarea {i}:\n"
             tarea_texto += f"Título: {tarea.titulo}\n"
             tarea_texto += f"Descripción: {tarea.descripcion}\n"
             tarea_texto += "Estado: Completo\n"
             tarea_texto += f"Fecha de Vencimiento: {tarea.fecha_vencimiento.strftime('%Y-%m-%d')}\n\n"
+          
 
     texto.config(text=tarea_texto)
     texto.pack()
@@ -127,11 +141,7 @@ def botones_opc(opcion):
         mostrar_tareas()
     elif opcion == 3:
         mostrar_estado_tareas()
-    elif opcion == 4:
-        editar_tareas()
 
-def editar_tareas():
-    print("a")
 def iniciobd():
     global conexion1,cursor1
     conexion1=mysql.connector.connect(host="localhost", user="root", passwd="", database="lpr")
@@ -172,12 +182,10 @@ def login():
         boton1 = Button(contenedor_botones, text="Agregar tareas", command=lambda: botones_opc(1))
         boton2 = Button(contenedor_botones, text="Mostrar tareas", command=lambda: botones_opc(2))
         boton3 = Button(contenedor_botones, text="Estado de tareas", command=lambda: botones_opc(3))
-        boton5 = Button(contenedor_botones, text="Editar tareas", command=lambda: botones_opc(4))
         boton4 = Button(contenedor_botones, text="Salir", command=lambda: ventana.quit())
         boton1.pack(side=LEFT, padx=10)
         boton2.pack(side=LEFT, padx=10)
         boton3.pack(side=LEFT, padx=10)
-        boton5.pack(side=LEFT, padx=10)
         boton4.pack(side=LEFT, padx=10)
 
         # Creacion de etiquetas y campos de entrada para tareas
@@ -206,14 +214,14 @@ usuario_label1=Label(text = "Nombre de usuario")
 usuario_login=Entry(ventana)
 contrasena_label1=Label(text = "Contraseña")
 contrasena_login=Entry(ventana)
-enviar = Button( text= "Enviar" ,command=login)
+enviar = Button( text= "enviar" ,command=login)
 
 
 usuario_label=Label(text = "Nombre de usuario")
 usuario_register=Entry(ventana)
 contrasena_label=Label(text = "Contraseña")
 contrasena_register=Entry(ventana)
-enviar_register = Button( text= "Enviar" ,command=register)
+enviar_register = Button( text= "enviar" ,command=register)
 
 
 def log_opc(opcion):
@@ -235,14 +243,15 @@ def log_opc(opcion):
         usuario_login.pack(pady=5)
         contrasena_label1.pack()
         contrasena_login.pack()
-        enviar.pack(pady=10)
+        enviar.pack()
+
     elif opcion==2:
         titulo.config(text="Registrate", font=('Helvaltica', 15))
         usuario_label.pack()
         usuario_register.pack(pady=5)
         contrasena_label.pack()
         contrasena_register.pack()
-        enviar_register.pack(pady=10)
+        enviar_register.pack()
 
 # Creacion de la ventana principal
 ventana.title("Trabajo programación")
@@ -256,6 +265,7 @@ boto4 = Button(contenedor_botones, text="Salir", command=lambda: ventana.quit())
 boto1.pack(side=LEFT, padx=10)
 boto2.pack(side=LEFT, padx=10)
 boto4.pack(side=LEFT, padx=10)
+
 
 # Iniciar la interfaz
 ventana.mainloop()
